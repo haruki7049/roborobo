12a04234b9dfb0bb4e305d0961610e9b70324205
haruki7049 <tontonkirikiri@gmail.com>
2025-03-11 23:43:54 +0900

    Update

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,41 @@
+use bevy::color::palettes::css;
 use bevy::prelude::*;
+use bevy::input::keyboard::keyboard_input_system;
+use bevy_vello::{VelloPlugin, prelude::*};
+use log::debug;
 
 fn main() {
     App::new()
         .add_plugins(DefaultPlugins)
+        .add_plugins(VelloPlugin::default())
         .add_systems(Startup, setup)
+        .add_systems(Update, (keyboard_input_system, keyboard_log))
+        .add_systems(Startup, setup_ui)
         .run();
 }
 
 fn setup(mut commands: Commands) {
     commands.spawn(Camera2d::default());
+}
+
+fn setup_ui(mut commands: Commands) {
+    let one_third = Val::Percent(100.0 / 3.0);
+    commands.spawn((
+        Node {
+            position_type: PositionType::Absolute,
+            left: one_third,
+            top: one_third,
+            width: one_third,
+            height: one_third,
+            border: UiRect::all(Val::Px(2.0)),
+            ..default()
+        },
+        BorderColor(css::FUCHSIA.with_alpha(0.5).into()),
+        Interaction::default(),
+        VelloScene::new(),
+    ));
+}
+
+fn keyboard_log(keyboard: Res<ButtonInput<KeyCode>>) {
+    debug!("keyboard: {:?}", keyboard.get_pressed().collect::<Vec<_>>());
 }
